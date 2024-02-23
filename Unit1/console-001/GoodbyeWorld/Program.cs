3dfdce00e85bc92048565c171e20c41297e7c209
Jose A. Fernandez <fernandezja@gmail.com>
2024-02-23 19:41:51 -0300

    [Unit1] Console001 (Hello/Goodbye World) .NET 8. Top-level statements

---
+++ Unit1/console-001/GoodbyeWorld/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Goodbye, World!");
+
+System.Console.WriteLine("...");
+System.Console.WriteLine("Press any key on the keyboard...!");
+System.Console.ReadKey();
+
---
+++ Unit1/console-001/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            System.Console.WriteLine("...");
+            System.Console.WriteLine("Press any key on the keyboard...!");
+            System.Console.ReadKey();
+
+        }
+    }
+}
