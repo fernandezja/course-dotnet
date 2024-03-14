76c4a8eafba2333f5246bae84ed8bb76e5435033
Jose A. Fernandez <fernandezja@gmail.com>
2024-03-14 16:29:48 -0300

    [Unit1] Demo Console-003

---
+++ Unit1/console-003/ConsoleApp1/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Person
+    {
+        public string Name { get; set; }
+			
+
+        private string _lastName;
+
+		public string LastName
+		{
+			get { 
+				return _lastName; 
+			}
+			set 
+			{ 
+				//Step1: Log into DB
+
+				//Step2:
+				_lastName = value; 
+			}
+		}
+
+
+		public int Age { get; set; } = 0;
+
+
+		public string FullName 
+		{
+			get 
+			{
+				return $"{Name} {LastName}";
+			}
+		}
+
+
+		public string FullName2() {
+            //return Name + " " + _lastName;
+            return Name + " " + LastName;
+        }
+
+
+
+    }
+}
---
+++ Unit1/console-003/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+Console.WriteLine("Hello, World!");
+
+//int a, b, c = 1;
+int a = 1;
+int b = 2;
+int c = 3;
+
+var d = "4";
+
+var intEdad = 1;
+
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+
+var p1 = new Person();
+p1.Name = "Person 100";
+
+Person p2 = new()
+{
+    Name = "Maria",
+    LastName = "MM"
+};
+
+var people = new List<Person>();
+people.Add(p1);
+
+
+
+
+
