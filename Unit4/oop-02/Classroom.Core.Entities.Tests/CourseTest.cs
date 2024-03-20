a892cb4c3017ebff9b7bb12b5b5300ed2bcd7d2e
Jose A. Fernandez <fernandezja@gmail.com>
2024-03-20 09:41:31 -0300

    [Unit4][oop-3] Course class

---
+++ Unit4/oop-02/Classroom.Core.Entities.Tests/CourseTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classroom.Core.Entities.Tests
+{
+    public class CourseTest
+    {
+        [Fact]
+        public void Test1() 
+        {
+
+            var course1 = new Course();
+            course1.Title = "NET with C#";
+
+            Assert.NotNull(course1);
+            Assert.Equal("NET with C#", course1.Title);
+        }
+
+
+        [Fact]
+        public void Test2()
+        {
+
+            var course1 = new Course();
+
+            course1.Teacher = new Teacher() { 
+                FirstName = "Lionel"
+            };
+
+            Assert.Equal("Lionel", course1.Teacher.FirstName);
+            Assert.Equal("Lionel", course1.Teacher.FullName);
+        }
+    }
+}
---
+++ Unit4/oop-02/Classroom.Core.Entities/Course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classroom.Core.Entities
+{
+    public class Course
+    {
+        public string Title { get; set; }
+        public Teacher Teacher { get; set; }
+    }
+}
