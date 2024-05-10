ddc1f816e9ba86ec16aa750b8005baeb748c469c
Jose A. Fernandez <fernandezja@gmail.com>
2024-05-10 19:05:47 -0300

    [Unit1][console-005]

---
+++ Unit1/console-005/ConsoleApp1Net8/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1Net8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("NET 8");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Unit1/console-005/ConsoleApp1NetFw/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1NetFw
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("NET Framework 4.8");
+
+            Console.ReadKey();
+        }
+    }
+}
