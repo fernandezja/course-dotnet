e211d10a48dc1690a208f896b2a434b41d6ddfe9
Jose A. Fernandez <fernandezja@gmail.com>
2024-05-16 17:18:50 -0300

    [BonusTrack][aspnet-mvc-01]

---
+++ BonusTrack/aspnet-mvc-01/WebApplication1/Controllers/JediController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class JediController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            var jedi = new Jedi { 
+                JediId = id,
+                Name = $"Jedi <br /> {id}"
+            };
+
+            return View("DetailsInternal", jedi);
+        }
+    }
+}
---
+++ BonusTrack/aspnet-mvc-01/WebApplication1/Models/Jedi.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Models
+{
+    public class Jedi
+    {
+        public int JediId { get; set; }
+        public string Name { get; set; }
+    }
+}
