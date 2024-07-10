51f6d769909b6b43c699b7b07311b533931703f2
Jose A. Fernandez <fernandezja@gmail.com>
2024-07-10 19:16:38 -0300

    [Unit8] CustomException, try/catch, Debug

---
+++ Unit8/release-01/ConsoleApp1/ClientApi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class ClientApi
+    {
+        public string GetValue(bool hasError) 
+        {
+            if (hasError)
+            {
+                throw new CustomException();
+            }
+
+            return "Un valor";
+        }
+    }
+}
--- Unit8/release-01/ConsoleApp1/Program.cs
+++ Unit8/release-01/ConsoleApp1/Program.cs
@@ -1,4 +1,57 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
+using ConsoleApp1;
+
 Console.WriteLine("Hello, World!");
 
-#error version
+var a = 0;
+
+Console.WriteLine(a);
+
+var c = System.Text.Encoding.ASCII.GetBytes("NET");
+
+
+var client = new ClientApi();
+
+try
+{
+    var d = client.GetValue(hasError: true);
+
+    Console.WriteLine($"Result: {d}");
+}
+catch (CustomException ex)
+{
+    Console.WriteLine($"Custom Error: {ex.Message}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"ERROR: {ex.Message}");
+}
+finally { 
+    //acciones... 
+}
+
+
+#if DEBUG
+    Console.WriteLine("Debug DEBUG");
+#endif
+
+#if RELEASE
+    Console.WriteLine("Debug RELEASE");
+#endif
+
+//try
+//{
+
+//}
+//catch (Exception)
+//{
+
+//	throw;
+//}
+
+////TODO: Revisar
+//var b = 1 / a;
+
+Console.WriteLine(a);
