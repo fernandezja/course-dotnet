7eb39e4d90bb0e901f699213fd987d60251cfd82
Jose A. Fernandez <fernandezja@gmail.com>
2024-09-26 16:10:18 -0300

    [Unit1][console-009]

---
+++ Unit1/console-009/ConsoleApp9v1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+ Multiple lineas de comentarios
+ */
+string name = "C#";
+int year = 2024;
+
+Console.WriteLine($"Hello, World! {name} {year}");
---
+++ Unit1/console-009/ConsoleApp9v2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp9v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "C#";
+            int year = 2024;
+
+            Console.WriteLine($"Hello, World! {name} {year}");
+        }
+    }
+}
