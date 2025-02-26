f1c9778279520bc2f2971b2e52df48511fe85f95
Jose A. Fernandez <jfernandez@desarrollosnea.com>
2025-02-25 21:14:51 -0300

    [Unit1][console-010]

---
+++ Unit1/console-010/ConsoleApp10/Program.cs
@@ -0,0 +1,5 @@
+﻿using System;
+
+// See https://aka.ms/new-console-template for more information
+  Console.WriteLine("Hello, World! 1");
+Console.WriteLine("Hello, World .NET! 2");
---
+++ Unit1/console-010/ConsoleApp10v2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp10v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! v2");
+        }
+    }
+}
