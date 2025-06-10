2ec14591f411041e5f93484a6cc8407f5ddd17ee
Jose A. Fernandez <jfernandez@desarrollosnea.com>
2025-06-10 20:10:49 -0300

    [Unit1][console-012]

---
+++ Unit1/console-012/ConsoleApp12Bye/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp12Bye
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bye, World!");
+        }
+    }
+}
